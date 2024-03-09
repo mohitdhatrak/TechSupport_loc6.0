52bf554fb6d952a2a2d2e00bff8b637b9d70096f
aradhyasakalley <sakalleyaradhya@gmail.com>
2024-03-09 17:14:48 +0530

    transter

--- backend/scrapers/ebay.py
+++ backend/scrapers/ebay.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pprint
 
 def ebay_scraper(url):
     headers = {
@@ -47,92 +46,7 @@ def ebay_scraper(url):
 
     return result
 
-def flipkart_scraper(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Connection': 'keep-alive'
-    }
+url = "https://www.ebay.com/itm/196119527515?_trkparms=amclksrc%3DITM%26aid%3D1110006%26algo%3DHOMESPLICE.SIM%26ao%3D1%26asc%3D20231107084023%26meid%3Dc68c6a801c474e5381e775c561f5e7d2%26pid%3D101875%26rk%3D1%26rkt%3D4%26sd%3D155780190004%26itm%3D196119527515%26pmt%3D1%26noa%3D1%26pg%3D4429486%26algv%3DSimplAMLv11WebTrimmedV3MskuWithLambda85KnnRecallV1V2V4ItemNrtInQueryAndCassiniVisualRankerAndBertRecallWithVMEV3CPCAutoWithCassiniEmbRecall%26brand%3DApple&_trksid=p4429486.c101875.m1851&itmprp=cksum%3A196119527515c68c6a801c474e5381e775c561f5e7d2%7Cenc%3AAQAIAAABQHD%252FO%252BVoFoPPIoZ2g0kOZxWd85mWuIHekSp3qag7zFfwObZeQoitzE%252FDCfcejfO%252BzOLmzZmy11RrgWpA56KM9DpasK%252BGtGYwCFvgaK6ijP5AqShEUhT8f2oX6hvndJ3G0oWH8DD1DlBWseQltpxcSDW2Uu52NrseaxeINpsrDP6vx7ny%252BkPKauMa3yeSanWiilC5jqhfIQu1hHPSl0Y0TKGCtR3Inlo260BOmOFneCK0cC5eeZFCmJBUEuCKWDGlNgRqXCt6nn7eWKrj1zys87cpHxBRQXwjPR%252B9rBcF3Yy5T8ODcmwWhRexO2AqL6SLiGRxfIcTYPFJu9nFxwTeFhGj1FI3bpdwzcwVv1C8fzXGqGxTE8KOgE7emLQhsT39%252BubDt1jPe7HuBI1nyvsOgNN4nJm5FucAZyZYrPbwn3qj%7Campid%3APLP_CLK%7Cclp%3A4429486&epid=3065007322&itmmeta=01HRH3TJXR2YWH8H4CQNPZXSKP"
+product_data = ebay_scraper(url)
+print(product_data)
 
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    
-    data = {'source': 'Flipkart'}
-
-    title = soup.find('h1', class_="yhB1nd").text
-    data['title'] = title
-
-    img_tag = soup.find('img', class_='_396cs4 _2amPTt _3qGmMb')
-    image_src = img_tag['src']
-    data['image'] = image_src
-
-    value = soup.find('span', class_="_2_R_DZ").text.strip()
-    split = value.split("&")
-    ratings = split[0].split()[0]
-    reviews = split[1].split()[0]
-    # data['ratings'] = ratings
-    data['reviews'] = reviews
-
-    stars = soup.find('div', class_="_3LWZlK").text
-    # data['stars'] = stars
-
-    price = soup.find('div', class_="_30jeq3 _16Jk6d").text
-    data['price'] = price
-
-    highlights_div = soup.find('div', class_='_2cM9lP')
-    if highlights_div:
-        highlights_title_div = highlights_div.find('div', class_='_3a9CI2')
-        highlights_ul = highlights_div.find('ul')
-        highlights_title = highlights_title_div.text.strip() if highlights_title_div else None
-        highlights_list = [li.text.strip() for li in highlights_ul.find_all('li')] if highlights_ul else []
-        data['description'] = highlights_list
-
-    return data
-
-def extract_search_results(query, platform):
-    params = {
-        "q": f"Buy {query} at best price {platform}",
-        "api_key": "2145d6d7b13649473c8fc27db3144a1fcd104d599cb85342faefc7e612e243ca"  # Replace this with your actual SerpApi API key
-    }
-
-    search = GoogleSearch(params)
-    results = search.get_dict()
-
-    # Extracting organic search results
-    organic_results = results.get("organic_results", [])
-
-    # Filtering and printing titles and URLs of organic search results from the specified platform
-    flipkart_links = []
-    ebay_links = []
-    indiamart_links = []
-    for result in organic_results:
-        link = result.get("link")
-        if link:
-            if "/p/" in link or "/buy/" in link or "/proddetail" in link or "/itm" in link:
-                if "flipkart" in link:
-                    flipkart_links.append(link)
-                elif "ebay" in link:
-                    ebay_links.append(link)
-                elif "indiamart" in link:
-                    indiamart_links.append(link)
-
-    return flipkart_links, ebay_links, indiamart_links
-
-queries = ["Iphone 15"]
-platforms = ["flipkart", "ebay", "indiamart"]
-
-for product in queries:
-    for platform in platforms:
-        print(f"Results for: {platform} {product}")
-        flipkart_links, ebay_links, indiamart_links = extract_search_results(product, platform)
-        if platform == "flipkart":
-            for link in flipkart_links:
-                product_data = flipkart_scraper(link)
-                pprint.pprint(product_data)
-        elif platform == "ebay":
-            for link in ebay_links:
-                product_data = ebay_scraper(link)
-                pprint.pprint(product_data)
-        print("\n")
